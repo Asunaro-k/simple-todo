ac900c4482ad3c9006dac3c99d24c75a07d14529
Asunaro-k <owarigami@gmail.com>
2020-10-15 16:43:47 +0900

    Updates

---
+++ precache-manifest.96e7f7de437e4d5d38bbf7de8b8788c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4db547b858f98b8348a5af3f643c4f3",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "cc1496d5532579bd62f6",
+    "url": "/simple-todo/static/css/main.776d571a.chunk.css"
+  },
+  {
+    "revision": "48743d0ba2832778c5fa",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1496d5532579bd62f6",
+    "url": "/simple-todo/static/js/main.68720493.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
